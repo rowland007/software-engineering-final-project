7f729400c6273b5dc69c527666a0af6db2fedbaa
rowland007 <rowland007@gmail.com>
2014-09-08 00:55:22 -0400

    Added comments to class files.

--- software engineering final project/employee.h
+++ software engineering final project/employee.h
@@ -1,3 +1,25 @@
+ /************************************************************************
+   Program: Software Engineering Final Project
+   Author: Randall D. Rowland Jr.
+   Class: employee
+   Instructor:
+   Date: 7 September 2014
+   Description:
+   Input:
+   Output: 
+   Compilation instructions:
+   Usage:
+   Known bugs/missing features:
+   
+   This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+   International Public License. To view a copy of this license, visit 
+   https://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative 
+   Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+   
+   Modifications:
+   Date                Comment            
+   ----    ------------------------------------------------
+ ************************************************************************/
 #pragma once
 #include "person.h"
 #include <ctime>
--- software engineering final project/person.h
+++ software engineering final project/person.h
@@ -1,3 +1,25 @@
+ /************************************************************************
+   Program: Cookie
+   Author: Randall D. Rowland Jr.
+   Class:
+   Instructor:
+   Date: 9 April 2012
+   Description:    (program requirements)
+   Input: cookie.txt
+   Output: report.txt
+   Compilation instructions:
+   Usage:
+   Known bugs/missing features:
+   
+   This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 
+   Unported License. To view a copy of this license, visit 
+   http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative 
+   Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+   
+   Modifications:
+   Date                Comment            
+   ----    ------------------------------------------------
+ ************************************************************************/
 #pragma once
 
 #include <string>
